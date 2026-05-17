e2143b96f87d43479e2e843cf97c3319f85a55cb
anzwkk <anam89737@gmail.com>
2026-05-17 22:52:46 +0300

    add DatabaseConnection with environment variables

--- src/main/java/com/anya/eventboard/db/DatabaseConnection.java
+++ src/main/java/com/anya/eventboard/db/DatabaseConnection.java
@@ -1,5 +1,17 @@
 package com.anya.eventboard.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    public static final String DB_URL = System.getenv("db_url");
+    public static final String DB_USERNAME = System.getenv("db_username");
+    public static final String DB_PASSWORD = System.getenv("db_password");
+
+    //отдает подключ к другим классам
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
 
 }
